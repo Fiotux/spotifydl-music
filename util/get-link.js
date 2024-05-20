cf35b7ce9a8953632fd0d9be623e002de7f73fd6
Fiotux <74451820+Fiotux@users.noreply.github.com>
2024-05-20 03:37:55 +0200

    changed to youtube music search
    changed get-link to use youtube music for searching and changed package lock

--- util/get-link.js
+++ util/get-link.js
@@ -1,51 +1,21 @@
-import { promisify } from 'util';
-import YoutubeSearch from 'yt-search';
+import Innertube from 'youtubei.js';
 import StringSimilarity from 'string-similarity';
-import Constants from './constants.js';
 import { logInfo } from './log-helper.js';
 import { generateTemplateString } from './format-generators.js';
 
-const {
-  YOUTUBE_SEARCH: { MAX_MINUTES },
-  INPUT_TYPES: { SONG },
-} = Constants;
-const search = promisify(YoutubeSearch);
+const youtube = await Innertube.create();
+const music = youtube.music;
 
 /**
  * This function does the actual api calls to youtube
  *
  * @param {String} searchTerms string to search on youtube with
- * @param {String} type the type of item being searched
- * @param {String[]} exclusionFilters exclusion texts for description, title
  * @returns {String[]} youtube links
  */
-const findLinks = async (searchTerms, type, exclusionFilters) => {
+const findLinks = async (searchTerms) => {
   logInfo(`searching youtube with keywords "${searchTerms}"`);
-  const result = await search(searchTerms);
-  const isSong = Object.values(SONG).includes(type);
-  return result.videos
-    .filter(
-      video =>
-        !exclusionFilters ||
-        !(
-          exclusionFilters.some(exclusionFilter =>
-            video.title.includes(exclusionFilter),
-          ) ||
-          exclusionFilters.some(exclusionFilter =>
-            video.description.includes(exclusionFilter),
-          )
-        ),
-    )
-    .filter(
-      video =>
-        (!isSong || video.seconds < MAX_MINUTES * 60) && video.seconds > 0,
-    )
-    .slice(0, 10)
-    .map(video =>
-      video.url.includes('https://youtube.com')
-        ? video.url
-        : 'https://youtube.com' + video.url,
-    );
+  const result = await music.search(searchTerms, {type:'song'});
+  return result.contents[2].contents[0].id
 };
 
 /**
@@ -55,9 +25,6 @@ const findLinks = async (searchTerms, type, exclusionFilters) => {
  * @param {String} itemName name of song
  * @param {String} albumName name of album
  * @param {String} artistName name of artist
- * @param {String} extraSearch extra search terms
- * @param {String} type type of download being requested
- * @param {String[]} exclusionFilters exclusion texts for description, title
  * @returns {String[]} youtube links
  */
 const getLinks = async ({
@@ -66,38 +33,29 @@ const getLinks = async ({
   artistName,
   extraSearch,
   searchFormat,
-  type,
-  exclusionFilters,
 }) => {
-  let links = [];
+  let musicLink = [];
   if (searchFormat.length) {
-    links = await findLinks(
-      generateTemplateString(itemName, albumName, artistName, searchFormat),
-      type,
-      exclusionFilters,
-    );
+    const templateString = await generateTemplateString(itemName, albumName, artistName, searchFormat);
+    musicLink = ["https://youtube.com/watch?v="+findLinks(templateString)];
   }
+
   // custom search format failed or was never provided try the generic way
-  if (!links.length) {
+  if (!musicLink.length) {
     const similarity = StringSimilarity.compareTwoStrings(itemName, albumName);
     // to avoid duplicate song downloads
     extraSearch = extraSearch ? ` ${extraSearch}` : '';
     if (similarity < 0.5) {
-      links = await findLinks(
-        `${albumName} - ${itemName}${extraSearch}`,
-        type,
-        exclusionFilters,
-      );
+      musicLink = await findLinks(
+        `${albumName} - ${itemName}${extraSearch}`);
     }
-    if (!links.length) {
-      links = await findLinks(
-        `${artistName} - ${itemName}${extraSearch}`,
-        type,
-        exclusionFilters,
-      );
+    if (!musicLink.length) {
+      musicLink = await findLinks(
+        `${artistName} - ${itemName}${extraSearch}`);
     }
   }
-  return links;
-};
+
+return musicLink;
+}
 
 export default getLinks;
